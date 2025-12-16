1edce1183c5c53e754b51a9d625a8aedc7a0d4af
Amin Zibayi <43344360+AminZibayi@users.noreply.github.com>
2025-12-16 09:54:52 +0330

    fix: github pages deploy

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,13 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // Set the base path to your repository name
+  // Note: Do NOT add a trailing slash (e.g., "/my-repo", not "/my-repo/")
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   reactStrictMode: true,
   images: {
+    // GitHub Pages cannot optimize images on the fly.
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
